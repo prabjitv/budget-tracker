825ec3b8c0a31b51ee16d9c845897da8c00579c7
prab <58503954+prabjitv@users.noreply.github.com>
2020-06-12 17:08:15 -0700

    created components and Singlepage page

---
+++ budgettracker/src/pages/Singlepage/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import logo from '../../logo.svg';
+
+
+const Singlepage = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/Apyp.js</code> and save to reload.
+</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+</a>
+    </header>
+  )
+};
+
+export default Singlepage;
