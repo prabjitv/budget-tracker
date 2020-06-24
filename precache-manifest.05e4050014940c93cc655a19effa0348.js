66ba8e884a6953ba60b72b9def1d8f9b9a0d185d
prab <58503954+prabjitv@users.noreply.github.com>
2020-06-23 17:52:48 -0700

    Updates

---
+++ precache-manifest.05e4050014940c93cc655a19effa0348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bf992f9dfc5354ccd5453a4119ffa4",
+    "url": "/budget-tracker/index.html"
+  },
+  {
+    "revision": "bb50c4944f6213a0d31b",
+    "url": "/budget-tracker/static/css/main.0a3f2cc4.chunk.css"
+  },
+  {
+    "revision": "f04722ee9cb37dad0bdc",
+    "url": "/budget-tracker/static/js/2.7617ac49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-tracker/static/js/2.7617ac49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb50c4944f6213a0d31b",
+    "url": "/budget-tracker/static/js/main.0e3281e0.chunk.js"
+  },
+  {
+    "revision": "30cffd275a53ad64c557",
+    "url": "/budget-tracker/static/js/runtime-main.e6200b13.js"
+  }
+]);
